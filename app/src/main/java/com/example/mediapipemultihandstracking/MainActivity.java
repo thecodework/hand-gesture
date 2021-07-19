890ba8996967639464b6671f9bd64fd936c24cb3
SonamAgrahari <sonamagrahari11@gmail.com>
2021-07-19 21:48:17 +0530

    Refactored code

--- app/src/main/java/com/example/mediapipemultihandstracking/MainActivity.java
+++ app/src/main/java/com/example/mediapipemultihandstracking/MainActivity.java
@@ -1,17 +1,3 @@
-// Copyright 2019 The MediaPipe Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.example.mediapipemultihandstracking;
 
 import android.content.Context;
@@ -24,10 +10,8 @@ import com.example.mediapipemultihandstracking.basic.BasicActivity;
 import com.google.mediapipe.formats.proto.LandmarkProto;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList;
-import com.google.mediapipe.formats.proto.RectProto;
 import com.google.mediapipe.framework.AndroidPacketCreator;
 import com.google.mediapipe.framework.Packet;
-import com.google.mediapipe.framework.PacketCallback;
 import com.google.mediapipe.framework.PacketGetter;
 
 import java.util.HashMap;
@@ -39,23 +23,17 @@ import java.util.Map;
  */
 public class MainActivity extends BasicActivity {
     private static final String TAG = "MainActivity";
-
-    private static final String OUTPUT_HAND_RECT = "hand_rects_from_palm_detections";
     private List<NormalizedLandmarkList> multiHandLandmarks;
-
     private static final String INPUT_NUM_HANDS_SIDE_PACKET_NAME = "num_hands";
     private static final String OUTPUT_LANDMARKS_STREAM_NAME = "hand_landmarks";
     // Max number of hands to detect/process.
     private static final int NUM_HANDS = 2;
-
     private TextView gesture;
-    private TextView moveGesture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gesture = findViewById(R.id.gesture);
-        moveGesture = findViewById(R.id.move_gesture);
 
         AndroidPacketCreator packetCreator = processor.getPacketCreator();
         Map<String, Packet> inputSidePackets = new HashMap<>();
@@ -65,71 +43,11 @@ public class MainActivity extends BasicActivity {
         processor.addPacketCallback(
                 OUTPUT_LANDMARKS_STREAM_NAME,
                 (packet) -> {
-                    //    Log.d(TAG, "Received multi-hand landmarks packet.");
                     multiHandLandmarks =
                             PacketGetter.getProtoVector(packet, NormalizedLandmarkList.parser());
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            gesture.setText(handGestureCalculator(multiHandLandmarks));
-                        }
-                    });
-                   /* Log.d(
-                            TAG,
-                            "[TS:"
-                                    + packet.getTimestamp()
-                                    + "] "
-                                    + getMultiHandLandmarksDebugString(multiHandLandmarks));*/
+                    runOnUiThread(() -> gesture.setText(handGestureCalculator(multiHandLandmarks)));
                 });
-        processor.addPacketCallback(
-                OUTPUT_HAND_RECT
-                , new PacketCallback() {
-                    @Override
-                    public void process(Packet packet) {
-
-                        List<RectProto.NormalizedRect> normalizedRectsList = PacketGetter.getProtoVector(packet, RectProto.NormalizedRect.parser());
-
-                        try {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    moveGesture.setText(handGestureMoveCalculator(normalizedRectsList));
-                                }
-                            });
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-    }
-
-    private String getMultiHandLandmarksDebugString(List<NormalizedLandmarkList> multiHandLandmarks) {
-        if (multiHandLandmarks.isEmpty()) {
-            return "No hand landmarks";
-        }
-        String multiHandLandmarksStr = "Number of hands detected: " + multiHandLandmarks.size() + "\n";
-        int handIndex = 0;
-        for (NormalizedLandmarkList landmarks : multiHandLandmarks) {
-            multiHandLandmarksStr +=
-                    "\t#Hand landmarks for hand[" + handIndex + "]: " + landmarks.getLandmarkCount() + "\n";
-            int landmarkIndex = 0;
-            for (NormalizedLandmark landmark : landmarks.getLandmarkList()) {
-                multiHandLandmarksStr +=
-                        "\t\tLandmark ["
-                                + landmarkIndex
-                                + "]: ("
-                                + landmark.getX()
-                                + ", "
-                                + landmark.getY()
-                                + ", "
-                                + landmark.getZ()
-                                + ")\n";
-                ++landmarkIndex;
-            }
-            ++handIndex;
-        }
-        return multiHandLandmarksStr;
     }
 
     private String handGestureCalculator(List<NormalizedLandmarkList> multiHandLandmarks) {
@@ -156,8 +74,6 @@ public class MainActivity extends BasicActivity {
                     thumbIsOpen = true;
                 }
             }
-            //  Log.d(TAG, "pseudoFixKeyPoint == " + pseudoFixKeyPoint + "\nlandmarkList.get(2).getX() == " + landmarkList.get(2).getX()
-            //          + "\nlandmarkList.get(4).getX() = " + landmarkList.get(4).getX());
             pseudoFixKeyPoint = landmarkList.get(6).getY();
             if (landmarkList.get(7).getY() < pseudoFixKeyPoint && landmarkList.get(8).getY() < landmarkList.get(7).getY()) {
                 firstFingerIsOpen = true;
@@ -191,10 +107,12 @@ public class MainActivity extends BasicActivity {
                 return "TWO";
             } else if (!thumbIsOpen && firstFingerIsOpen && !secondFingerIsOpen && !thirdFingerIsOpen && !fourthFingerIsOpen) {
                 Log.d(TAG, "One");
-                audioManager.adjustStreamVolume(
-                        AudioManager.STREAM_MUSIC,
-                        AudioManager.ADJUST_RAISE,
-                        AudioManager.FLAG_PLAY_SOUND | AudioManager.FLAG_SHOW_UI);
+                if (audioManager != null) {
+                    audioManager.adjustStreamVolume(
+                            AudioManager.STREAM_MUSIC,
+                            AudioManager.ADJUST_RAISE,
+                            AudioManager.FLAG_PLAY_SOUND | AudioManager.FLAG_SHOW_UI);
+                }
                 return "ONE";
             } else if (!thumbIsOpen && firstFingerIsOpen && secondFingerIsOpen && !thirdFingerIsOpen && !fourthFingerIsOpen) {
                 Log.d(TAG, "Yeah");
@@ -207,117 +125,22 @@ public class MainActivity extends BasicActivity {
                 return "Spider-Man";
             } else if (!thumbIsOpen && !firstFingerIsOpen && !secondFingerIsOpen && !thirdFingerIsOpen && !fourthFingerIsOpen) {
                 Log.d(TAG, "fist");
-                audioManager.adjustStreamVolume(
-                        AudioManager.STREAM_MUSIC,
-                        AudioManager.ADJUST_LOWER,
-                        AudioManager.FLAG_PLAY_SOUND | AudioManager.FLAG_SHOW_UI);
-
+                if (audioManager != null) {
+                    audioManager.adjustStreamVolume(
+                            AudioManager.STREAM_MUSIC,
+                            AudioManager.ADJUST_LOWER,
+                            AudioManager.FLAG_PLAY_SOUND | AudioManager.FLAG_SHOW_UI);
+                }
                 return "fist";
             } else if (!firstFingerIsOpen && secondFingerIsOpen && thirdFingerIsOpen && fourthFingerIsOpen && isThumbNearFirstFinger(landmarkList.get(4), landmarkList.get(8))) {
                 Log.d(TAG, "ok");
                 return "OK";
             } else {
-                String info = "thumbIsOpen " + thumbIsOpen + "firstFingerIsOpen" + firstFingerIsOpen
-                        + "secondFingerIsOpen" + secondFingerIsOpen +
-                        "thirdFingerIsOpen" + thirdFingerIsOpen + "fourthFingerIsOpen" + fourthFingerIsOpen;
-                //  Log.d(TAG, "handGestureCalculator: == " + info);
                 return "___";
             }
         }
         return "___";
     }
-
-    float previousXCenter;
-    float previousYCenter;
-    float previousAngle; // angle between the hand and the x-axis. in radian
-    float previous_rectangle_width;
-    float previousRectangleHeight;
-    boolean frameCounter;
-
-    private String handGestureMoveCalculator(List<RectProto.NormalizedRect> normalizedRectList) {
-
-        RectProto.NormalizedRect normalizedRect = normalizedRectList.get(0);
-        float height = normalizedRect.getHeight();
-        float centerX = normalizedRect.getXCenter();
-        float centerY = normalizedRect.getYCenter();
-        if (previousXCenter != 0) {
-            double mouvementDistance = getEuclideanDistanceAB(centerX, centerY,
-                    previousXCenter, previousYCenter);
-            // LOG(INFO) << "Distance: " << mouvementDistance;
-
-            double mouvementDistanceFactor = 0.02; // only large mouvements will be recognized.
-
-            // the height is normed [0.0, 1.0] to the camera window height.
-            // so the mouvement (when the hand is near the camera) should be equivalent to the mouvement when the hand is far.
-            double mouvementDistanceThreshold = mouvementDistanceFactor * height;
-            if (mouvementDistance > mouvementDistanceThreshold) {
-                double angle = radianToDegree(getAngleABC(centerX, centerY,
-                        previousXCenter, previousYCenter, previousXCenter + 0.1,
-                        previousYCenter));
-                // LOG(INFO) << "Angle: " << angle;
-                if (angle >= -45 && angle < 45) {
-                    return "Scrolling right";
-                } else if (angle >= 45 && angle < 135) {
-                    return "Scrolling up";
-                } else if (angle >= 135 || angle < -135) {
-                    return "Scrolling left";
-                } else if (angle >= -135 && angle < -45) {
-                    return "Scrolling down";
-                }
-            }
-        }
-
-        previousXCenter = centerX;
-        previousYCenter = centerY;
-        // 2. FEATURE - Zoom in/out
-        if (previousRectangleHeight != 0) {
-            double heightDifferenceFactor = 0.03;
-
-            // the height is normed [0.0, 1.0] to the camera window height.
-            // so the mouvement (when the hand is near the camera) should be equivalent to the mouvement when the hand is far.
-            double heightDifferenceThreshold = height * heightDifferenceFactor;
-            if (height < previousRectangleHeight - heightDifferenceThreshold) {
-                return "Zoom out";
-            } else if (height > previousRectangleHeight + heightDifferenceThreshold) {
-                return "Zoom in";
-            }
-        }
-        previousRectangleHeight = height;
-        // each odd Frame is skipped. For a better result.
-        frameCounter = !frameCounter;
-        if (frameCounter && multiHandLandmarks != null) {
-
-            for (NormalizedLandmarkList landmarks : multiHandLandmarks) {
-
-                List<NormalizedLandmark> landmarkList = landmarks.getLandmarkList();
-                NormalizedLandmark wrist = landmarkList.get(0);
-                NormalizedLandmark MCP_of_second_finger = landmarkList.get(9);
-
-                // angle between the hand (wirst and MCP) and the x-axis.
-                double ang_in_radian =
-                        getAngleABC(MCP_of_second_finger.getX(), MCP_of_second_finger.getY(),
-                                wrist.getX(), wrist.getY(), wrist.getX() + 0.1, wrist.getY());
-                int ang_in_degree = radianToDegree(ang_in_radian);
-                // LOG(INFO) << "Angle: " << ang_in_degree;
-                if (previousAngle != 0) {
-                    double angleDifferenceTreshold = 12;
-                    if (previousAngle >= 80 && previousAngle <= 100) {
-                        if (ang_in_degree > previousAngle + angleDifferenceTreshold) {
-                            return "Slide left";
-
-                        } else if (ang_in_degree < previousAngle - angleDifferenceTreshold) {
-                            return "Slide right";
-
-                        }
-                    }
-                }
-                previousAngle = ang_in_degree;
-            }
-
-        }
-        return "";
-    }
-
 
     private boolean isThumbNearFirstFinger(LandmarkProto.NormalizedLandmark point1, LandmarkProto.NormalizedLandmark point2) {
         double distance = getEuclideanDistanceAB(point1.getX(), point1.getY(), point2.getX(), point2.getY());
@@ -327,21 +150,5 @@ public class MainActivity extends BasicActivity {
     private double getEuclideanDistanceAB(double a_x, double a_y, double b_x, double b_y) {
         double dist = Math.pow(a_x - b_x, 2) + Math.pow(a_y - b_y, 2);
         return Math.sqrt(dist);
-    }
-
-    private double getAngleABC(double a_x, double a_y, double b_x, double b_y, double c_x, double c_y) {
-        double ab_x = b_x - a_x;
-        double ab_y = b_y - a_y;
-        double cb_x = b_x - c_x;
-        double cb_y = b_y - c_y;
-
-        double dot = (ab_x * cb_x + ab_y * cb_y);   // dot product
-        double cross = (ab_x * cb_y - ab_y * cb_x); // cross product
-
-        return Math.atan2(cross, dot);
-    }
-
-    private int radianToDegree(double radian) {
-        return (int) Math.floor(radian * 180. / Math.PI + 0.5);
     }
 }
