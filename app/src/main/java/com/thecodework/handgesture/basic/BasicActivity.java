d91471f01c7a0798d1ac117fdef2524df05753d6
SonamAgrahari <sonamagrahari11@gmail.com>
2022-09-12 18:44:01 +0530

    removed Log

--- app/src/main/java/com/thecodework/handgesture/basic/BasicActivity.java
+++ app/src/main/java/com/thecodework/handgesture/basic/BasicActivity.java
@@ -5,14 +5,12 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Size;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.mediapipe.components.CameraHelper;
 import com.google.mediapipe.components.CameraXPreviewHelper;
@@ -28,13 +26,6 @@ import com.thecodework.handgesture.R;
  */
 public class BasicActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-
-    // Flips the camera-preview frames vertically by default, before sending them into FrameProcessor
-    // to be processed in a MediaPipe graph, and flips the processed frames back when they are
-    // displayed. This maybe needed because OpenGL represents images assuming the image origin is at
-    // the bottom-left corner, whereas MediaPipe in general assumes the image origin is at the
-    // top-left corner.
-    // NOTE: use "flipFramesVertically" in manifest metadata to override this behavior.
     private static final boolean FLIP_FRAMES_VERTICALLY = true;
 
     static {
@@ -42,29 +33,16 @@ public class BasicActivity extends AppCompatActivity {
         try {
             System.loadLibrary("opencv_java3");
         } catch (UnsatisfiedLinkError e) {
-            // Some example apps (e.g. template matching) require OpenCV 4.
             System.loadLibrary("opencv_java4");
         }
     }
 
-    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
-    // frames onto a {@link Surface}.
     protected FrameProcessor processor;
-    // Handles camera access via the {@link CameraX} Jetpack support library.
     protected CameraXPreviewHelper cameraHelper;
-
-    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
     private SurfaceTexture previewFrameTexture;
-    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
     private SurfaceView previewDisplayView;
-
-    // Creates and manages an {@link EGLContext}.
     private EglManager eglManager;
-    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
-    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
     private ExternalTextureConverter converter;
-
-    // ApplicationInfo for retrieving metadata defined in the manifest.
     private ApplicationInfo applicationInfo;
     private TextView no_camera_access_view;
 
@@ -72,7 +50,7 @@ public class BasicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        no_camera_access_view=findViewById(R.id.no_camera_access_view);
+        no_camera_access_view = findViewById(R.id.no_camera_access_view);
         if (PermissionHelper.cameraPermissionsGranted(this)) {
             no_camera_access_view.setVisibility(View.GONE);
         }
@@ -84,9 +62,6 @@ public class BasicActivity extends AppCompatActivity {
 
         previewDisplayView = new SurfaceView(this);
         setupPreviewDisplayView();
-
-        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
-        // binary graphs.
         AndroidAssetUtil.initializeNativeAssetManager(this);
         eglManager = new EglManager(null);
         processor =
@@ -107,7 +82,6 @@ public class BasicActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        Log.d("camera","log call onResume");
         super.onResume();
         converter = new ExternalTextureConverter(eglManager.getContext());
         converter.setFlipY(
@@ -127,41 +101,29 @@ public class BasicActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(
             int requestCode, String[] permissions, int[] grantResults) {
-        Log.d("camera","log call onRequestPermissionsResult");
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     protected void onCameraStarted(SurfaceTexture surfaceTexture) {
-        Log.d("camera","log3 call onCameraStarted1");
         previewFrameTexture = surfaceTexture;
-        Log.d("camera","log  onCameraStarted2");
-        // Make the display view visible to start showing the preview. This triggers the
-        // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
         previewDisplayView.setVisibility(View.VISIBLE);
-        Log.d("camera","log  onCameraStarted3");
     }
 
     protected Size cameraTargetResolution() {
-        Log.d("camera","log call cameraTargetResolution");
-        return null; // No preference and let the camera (helper) decide.
+        return null;
     }
 
     public void startCamera() {
-        Log.d("camera","log2 call startCamera1");
         cameraHelper = new CameraXPreviewHelper();
-        Log.d("camera","log startCamera2");
         cameraHelper.setOnCameraStartedListener(
                 surfaceTexture -> onCameraStarted(surfaceTexture));
-        Log.d("camera","log startCamera3");
         CameraHelper.CameraFacing cameraFacing =
                 applicationInfo.metaData.getBoolean("cameraFacingFront", false)
                         ? CameraHelper.CameraFacing.FRONT
                         : CameraHelper.CameraFacing.BACK;
-        Log.d("camera","log startCamera4");
         cameraHelper.startCamera(
                 this, cameraFacing, /*surfaceTexture=*/ null, cameraTargetResolution());
-        Log.d("camera","log startCamera5");
     }
 
     protected Size computeViewSize(int width, int height) {
@@ -170,17 +132,9 @@ public class BasicActivity extends AppCompatActivity {
 
     protected void onPreviewDisplaySurfaceChanged(
             int width, int height) {
-        // (Re-)Compute the ideal size of the camera-preview display (the area that the
-        // camera-preview frames get rendered onto, potentially with scaling and rotation)
-        // based on the size of the SurfaceView that contains the display.
-        Log.d("camera","log6 call onPreviewDisplaySurfaceChanged");
         Size viewSize = computeViewSize(width, height);
         Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
         boolean isCameraRotated = cameraHelper.isCameraRotated();
-
-        // Connect the converter to the camera-preview frames as its input (via
-        // previewFrameTexture), and configure the output width and height as the computed
-        // display size.
         converter.setSurfaceTextureAndAttachToGLContext(
                 previewFrameTexture,
                 isCameraRotated ? displaySize.getHeight() : displaySize.getWidth(),
@@ -188,7 +142,6 @@ public class BasicActivity extends AppCompatActivity {
     }
 
     private void setupPreviewDisplayView() {
-        Log.d("camera","log1 call setupPreviewDisplayView ");
         previewDisplayView.setVisibility(View.GONE);
         ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
         viewGroup.addView(previewDisplayView);
@@ -199,19 +152,16 @@ public class BasicActivity extends AppCompatActivity {
                         new SurfaceHolder.Callback() {
                             @Override
                             public void surfaceCreated(SurfaceHolder holder) {
-                                Log.d("camera","log4 call surfaceCreated");
                                 processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
                             }
 
                             @Override
                             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                                Log.d("camera","log5 call surfaceChanged");
-                               onPreviewDisplaySurfaceChanged(width, height);
+                                onPreviewDisplaySurfaceChanged(width, height);
                             }
 
                             @Override
                             public void surfaceDestroyed(SurfaceHolder holder) {
-                                Log.d("camera","log call surfaceDestroyed");
                                 processor.getVideoSurfaceOutput().setSurface(null);
                             }
                         });
